991e2bfc31e84800490f7201830b0f48ae0b8a09
ErwinMull <122201580+ErwinMull@users.noreply.github.com>
2025-05-10 18:39:36 +0200

    adjust hemoglobin service to use reference range
    Use the reference range included in the Observation resource instead of
    hardcoded values

--- service/hemoglobin-check-service.rkt
+++ service/hemoglobin-check-service.rkt
@@ -2,34 +2,49 @@
 
 (provide check-hemoglobin)
 
-(define low 7.5)
-(define high 10)
+(define (extract-low&high reference-ranges)
+  (cond
+    [(null? reference-ranges) (values #f #f)]
+    [else (let* ([first-range (car reference-ranges)]
+                 [low-quantity (hash-ref first-range 'low (hasheq))]
+                 [low (hash-ref low-quantity 'value #f)]
+                 [high-quantity (hash-ref first-range 'high (hasheq))]
+                 [high (hash-ref high-quantity 'value #f)])
+            (values low high))]))
 
 (define (check-hemoglobin observation)
   (define value
     (let* ([tmp (hash-ref observation 'valueQuantity (hasheq))]
            [tmp (hash-ref tmp 'value #f)])
       tmp))
+  (define-values (low high)
+    (extract-low&high (hash-ref observation 'referenceRange '())))
   (define details (format "~a (low: ~a, high: ~a)" (or value "n/a") low high))
   (cond
     [(not value) (list
-                    (hasheq 'summary "No Blood Pressure found"
-                            'indicator "info"
-                            'details details
-                            'source (hasheq
-                                     'label "My service")))]
-    [(< value low) (list
-                    (hasheq 'summary "Blood Pressure: LOW"
-                            'indicator "warning"
-                            'details details
-                            'source (hasheq
-                                     'label "My service")))]
-    [(> value high) (list
-                     (hasheq 'summary "Blood Pressure: HIGH"
-                             'indicator "warning"
-                             'details details
-                             'source (hasheq
-                                      'label "My service")))]
+                  (hasheq 'summary "No Blood Pressure found"
+                          'indicator "info"
+                          'details details
+                          'source (hasheq
+                                   'label "My service")))]
+    [(and (not low) (not high)) (list
+                                 (hasheq 'summary "No Reference Range given"
+                                         'indicator "info"
+                                         'details details
+                                         'source (hasheq
+                                                  'label "My service")))]
+    [(and low (< value low)) (list
+                              (hasheq 'summary "Blood Pressure: LOW"
+                                      'indicator "warning"
+                                      'details details
+                                      'source (hasheq
+                                               'label "My service")))]
+    [(and high (> value high)) (list
+                                (hasheq 'summary "Blood Pressure: HIGH"
+                                        'indicator "warning"
+                                        'details details
+                                        'source (hasheq
+                                                 'label "My service")))]
     [else (list
            (hasheq 'summary "Blood Pressure: NORMAL"
                    'indicator "info"
